d98ded6f3635f86563520a8a84c8d908751ef518
ssk <sasat.s.93@gmail.com>
2021-01-20 22:40:03 +0900

    投票できない問題発生中

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from .models import Question
+from .models import Question, Choice
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
+from django.urls import reverse
 
 
 def index(request):
@@ -15,14 +16,26 @@ def index(request):
 
 
 def detail(request, question_id):
+    return HttpResponse('Hi')
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {question: question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html',
+                      {'question': question,
+                       'error_message': "Yout didn't select a choice."
+                       })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
