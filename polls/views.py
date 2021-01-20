45afdf15624411ac3008708382998bc43ccbab78
ssk <sasat.s.93@gmail.com>
2021-01-20 23:00:00 +0900

    投票成功

--- polls/views.py
+++ polls/views.py
@@ -16,14 +16,14 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse('Hi')
+    return HttpResponse('oh')
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {question: question})
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
