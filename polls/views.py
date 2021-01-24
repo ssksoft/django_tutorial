1a451888bb5ac7f9da5372f2cb0ba82ff6b65186
ssk <sasat.s.93@gmail.com>
2021-01-24 17:19:24 +0900

    Viewのテスト作成し実行した。合格した。 https://docs.djangoproject.com/ja/3.1/intro/tutorial05/

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.views import generic
 
 from .models import Question, Choice
+from django.utils import timezone
 
 
 class IndexView(generic.ListView):
@@ -11,7 +12,7 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
